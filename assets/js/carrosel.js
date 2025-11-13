9b3505c78417ee6cad4c96725ce9db3ee3321dd7
RicardoCiriloPicPay <ricardo.cirilo@picpay.com>
2025-11-13 17:01:56 -0300

    feat(carrosel): adiciona a função de carrosel automático ao acessar a página

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -6,21 +6,41 @@ document.addEventListener("DOMContentLoaded", () => {
     let activeDot = document.querySelector(".dot.active")
     let activeImg = document.querySelector(".hero-img.active");
     let activeContent = document.querySelector(".hero-content.active");
+    let activeIndex = 0;
+    let rodarAutomatico;
 
     dots.forEach((dot, i) => {
         dot.addEventListener("click", () => {
-            activeDot.classList.remove("active");
-            activeImg.classList.remove("active");
-            activeContent.classList.remove("active");
-
-            dot.classList.add("active");
-            imgs[i].classList.add("active");
-            contents[i].classList.add("active");
-
-            activeDot = dot;
-            activeImg = imgs[i];
-            activeContent = contents[i];
+            clearInterval(rodarAutomatico)
+            mudarAtivo(dot,  i);
         })
     })
 
+    function mudarAtivo(dot, i) {
+        activeDot.classList.remove("active");
+        activeImg.classList.remove("active");
+        activeContent.classList.remove("active");
+
+        dot.classList.add("active");
+        imgs[i].classList.add("active");
+        contents[i].classList.add("active");
+
+        activeDot = dot;
+        activeImg = imgs[i];
+        activeContent = contents[i];
+        activeIndex = i;
+
+    }
+    function mudarIndex(i){
+        return i >= dots.length - 1 ? 0 : i + 1;
+    }
+    function automatizarSlides() {
+        rodarAutomatico = setInterval(() => {
+            activeIndex = mudarIndex(activeIndex);
+
+            mudarAtivo(dots[activeIndex], activeIndex);
+        }, 5000);
+    }
+
+    automatizarSlides();
 })
